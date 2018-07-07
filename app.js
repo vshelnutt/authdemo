0a23d49df0eb021d15bfa256b485d3607c7e5b97
Vincent Shelnutt <extremenimbochromis@yahoo.com>
2018-07-07 15:23:00 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/auth_demo_app');
+
+let app = express();
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get("/secret", (req, res) => {
+    res.render('secret');
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log("server started.......");
+})
